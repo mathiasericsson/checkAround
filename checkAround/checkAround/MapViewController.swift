b3e40b72e7bcb2a4e8552ae0238c978452e4a5ea
Mathias Ericsson <mathiasericsson@MacBook-Pro.nodeone.kb11>
2016-11-10 14:54:29 +0100

    Fixed zooming of map and updated icon

--- checkAround/checkAround/MapViewController.swift
+++ checkAround/checkAround/MapViewController.swift
@@ -11,23 +11,25 @@ import MapKit
 import CoreLocation
 
 
+
 class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerDelegate {
 
     @IBOutlet weak var mapView: MKMapView!
     var locationManager:CLLocationManager?
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //self.locationManager = UserLocation.sharedInstance.locationManager
-        self.mapView.showsUserLocation = true
+        locationManager = CLLocationManager()
         locationManager?.delegate = self
         locationManager?.desiredAccuracy = kCLLocationAccuracyBest
         locationManager?.requestWhenInUseAuthorization() //To only use GPS when app is active, and not in background
-        //locationManager?.distanceFilter = 0
+        locationManager?.distanceFilter = 5
         locationManager?.startUpdatingLocation()
-
+        
+        self.mapView.showsUserLocation = true
     }
+    
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
@@ -35,20 +37,14 @@ class MapViewController: UIViewController, MKMapViewDelegate, CLLocationManagerD
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-
-        print("Updated location")
+ 
         let location = locations.last
         let center = CLLocationCoordinate2D(latitude: location!.coordinate.latitude, longitude: location!.coordinate.longitude)
         
-        let span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
-        
+        //Zoom in map
+        let span = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
         let region = MKCoordinateRegion(center:center, span:span)
-    
-        print("Hej")
-    
         self.mapView.setRegion(region, animated: true)
-    
-        print("zoom")
         self.locationManager?.stopUpdatingLocation()
         
     }
